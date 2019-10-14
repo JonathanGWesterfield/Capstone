ab571f7ba78635577a099ea3072ecff47e1ef779
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-13 19:48:00 -0500

    Added an Enumeration of valid network signals

---
+++ Camera-App/app/src/main/java/com/example/camera_app/Signal.java
@@ -0,0 +1,9 @@
+package com.example.camera_app;
+
+/**
+ * Enumeration for different signals that can be handled by this app.
+ */
+public enum Signal
+{
+    START, STOP, START_ACKNOWLEDGE, STOP_ACKNOWLEDGE, FTP_STARTED, FTP_COMPLETED
+}
