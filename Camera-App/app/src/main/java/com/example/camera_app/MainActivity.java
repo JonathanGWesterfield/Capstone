faa175c32230afbcaddd74799049e9cb5037c6b1
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-13 19:46:41 -0500

    Created the first activity view

--- Camera-App/app/src/main/java/com/example/camera_app/MainActivity.java
+++ Camera-App/app/src/main/java/com/example/camera_app/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.camera_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * This main activity will then redirect to the camera activity once a tcp connection has
+ * been established between the phone and the laptop.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    String ipaddr;
+    int port;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Handles the click event for the connect button. Once clicked, the phone should take
+     * the IP address in the IP address textbox and store the connection info for the laptop.
+     * Then the phone should open a TCP connection to the laptop to receive control signals.
+     */
+    public void btnConnClk(View view)
+    {
+
     }
 }
