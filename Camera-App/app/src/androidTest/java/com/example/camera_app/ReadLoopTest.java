630fb6b12ddacbf72d2fbb2898138b98a9a0e0fe
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-18 18:53:20 -0500

    Implemented the ReadLoop class. Created an empty JUnit test for it

---
+++ Camera-App/app/src/androidTest/java/com/example/camera_app/ReadLoopTest.java
@@ -0,0 +1,36 @@
+package com.example.camera_app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReadLoopTest {
+
+    @Test
+    public void addObserver() {
+    }
+
+    @Test
+    public void removeObserver() {
+    }
+
+    @Test
+    public void removeAllObservers() {
+    }
+
+    @Test
+    public void notifyObservers() {
+    }
+
+    @Test
+    public void parseSignal() {
+    }
+
+    @Test
+    public void parseMessage() {
+    }
+
+    @Test
+    public void run() {
+    }
+}
