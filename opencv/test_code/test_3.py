c52f9fddb57e84a3848541f25d1cf12951af05f7
Ismael <rismael57@gmail.com>
2019-10-24 23:41:19 -0500

    Attempting to find specific color. Added some coomments.

--- opencv/test_code/test_3.py
+++ opencv/test_code/test_3.py
@@ -6,17 +6,18 @@ print("Major version : {0}".format(major_ver))
 print("Minor version : {0}".format(minor_ver))
 print("Subminor version : {0}".format(subminor_ver))
 
-
-image = cv2.imread("color_wheel.jpeg")
-
+#get image
+image = cv2.imread("sunlight.jpg")
+#convert image from BGR to HSV to accurately find color
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-lower_red = np.array([0, 100, 100])
-upper_red = np.array([10, 255, 255])
-mask = cv2.inRange(hsv, lower_red, upper_red)
-
+#HSV color value bounds
+lower_yellow = np.array([0, 100, 100])
+upper_yellow = np.array([46, 255, 255])
+#find area in hsv that match boundes
+mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+#show images
 cv2.imshow("Original Image", image)
 cv2.imshow("HSV", hsv)
-cv2.imshow("Detected Red", mask)
+cv2.imshow("Detected Color", mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
