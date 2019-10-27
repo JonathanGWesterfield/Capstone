6931c364a6129e313fa38f9192266c80c928b88e
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-27 17:39:56 -0500

    Added command line args so we can either listen or send first with the TestServer. Needed to do this to properly test the network stuff with the phones

--- Server-Module/venv/src/TestServer.py
+++ Server-Module/venv/src/TestServer.py
@@ -5,6 +5,7 @@ import datetime
 # from Signal import Signals
 import sys
 import os
+import argparse
 
 class Server:
     """
@@ -71,6 +72,17 @@ class Server:
 
         self.conn.sendall(command)
 
+
+    def listen(self):
+        """
+        Listens to the other side of the connection for any message that might get sent through.
+        :return: The message that was received from the other side.
+        """
+        if self.conn is None:
+            print("ERROR! NEED TO CONNECT TO PHONE FIRST")
+            return
+
+        print("Listening!\n")
         # Wait for the acknowledgement
         message = self.conn.recv(1024)
 
@@ -79,10 +91,21 @@ class Server:
 
         message = message.decode('utf-8').strip()
 
-        print("Returned: ", message)
+        return message
 
 
-def main():
+def listen():
+    """
+    Works the same as send but in reverse. Will listen for connections first THEN, allow the user to start sending other
+    messages to the phone.
+    :return: None
+    """
+    server = Server(8000)
+    while(True):
+        print("Returned: ", server.listen())
+        server.sendCommand(input("Send: "))
+
+def send():
     """
     Take user input for what to send to the server until user kills the program
     :return: None
@@ -91,6 +114,31 @@ def main():
 
     while(True):
         server.sendCommand(input("Send: "))
+        print("Returned: ", server.listen())
+
+
+def main():
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--listen", help="Start the server in listening mode", action="store_true")
+    parser.add_argument("-s", "--send", help="Start the server in sending mode", action="store_true")
+    args = parser.parse_args()
+
+    if args.listen:
+        print("Started up in listening mode")
+        listen()
+        return
+
+    if args.send:
+        print("Started up in sending mode")
+        send()
+
+
+
+
+
+
 
 if __name__ ==  '__main__':
     main()
